315fcfd5a9f5ae69f4260cac88323314d7d14949
alex <alex@example.com>
2025-12-11 12:56:53 +0100

    fix: correct path for rgb_power_underglow source file in board.cmake

--- boards/arm/sofle_choc_pro/board.cmake
+++ boards/arm/sofle_choc_pro/board.cmake
@@ -7,4 +7,4 @@ include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
 
 zephyr_library()
-zephyr_library_sources(rgb_power_underglow.c)
+zephyr_library_sources(${CMAKE_CURRENT_LIST_DIR}/rgb_power_underglow.c)
