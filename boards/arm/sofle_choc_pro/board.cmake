47b4776355cfc8d304d5e8b9cc45741715820c2b
alex <alex@example.com>
2025-12-11 12:54:12 +0100

    feat: implement RGB underglow management for USB power state

--- boards/arm/sofle_choc_pro/board.cmake
+++ boards/arm/sofle_choc_pro/board.cmake
@@ -5,3 +5,6 @@ set(OPENOCD_NRF5_SUBFAMILY nrf52)
 board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
+
+zephyr_library()
+zephyr_library_sources(rgb_power_underglow.c)
