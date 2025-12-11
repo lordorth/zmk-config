5648beb3380a83caa21b25ef1a0c2bdd2dcc9dac
alex <alex@example.com>
2025-12-11 13:11:26 +0100

    feat: add include directory for ZMK application in board configuration

--- boards/arm/sofle_choc_pro/board.cmake
+++ boards/arm/sofle_choc_pro/board.cmake
@@ -8,3 +8,4 @@ include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
 
 zephyr_library()
 zephyr_library_sources(${CMAKE_CURRENT_LIST_DIR}/rgb_power_underglow.c)
+zephyr_library_include_directories(${ZEPHYR_BASE}/../zmk/app/include)
