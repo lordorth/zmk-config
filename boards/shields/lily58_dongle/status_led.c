7b398bb6c825fbdd88807f3203a49b695292a197
Alexander Orth <alexander.orth@rub.de>
2026-01-10 11:16:48 +0100

    feat: add Lily58 Dongle support with firmware preparation and configuration

---
+++ boards/shields/lily58_dongle/status_led.c
@@ -0,0 +1,24 @@
+/*
+ * Force the nice!nano blue LED off on the dongle to avoid blinking.
+ */
+#include <zephyr/device.h>
+#include <zephyr/drivers/led.h>
+#include <zephyr/init.h>
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(lily58_dongle_led, CONFIG_ZMK_LOG_LEVEL);
+
+#if DT_NODE_EXISTS(DT_ALIAS(led0))
+static const struct device *const status_led = DEVICE_DT_GET(DT_ALIAS(led0));
+#endif
+
+static int led_init(const struct device *dev) {
+    ARG_UNUSED(dev);
+#if DT_NODE_EXISTS(DT_ALIAS(led0))
+    if (device_is_ready(status_led)) {
+        led_off(status_led, 0);
+    }
+#endif
+    return 0;
+}
+
+SYS_INIT(led_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
